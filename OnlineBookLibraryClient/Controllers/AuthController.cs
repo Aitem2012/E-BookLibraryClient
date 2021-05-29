5a5d2811fec37ba3348ed1b66f02acdae83f4f6a
Aitem2012 <ibrahim.ajumoko@decagon.dev>
2021-05-29 14:23:00 +0100

    initial commit

---
+++ OnlineBookLibraryClient/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBookLibraryClient.Controllers
+{
+    public class AuthController :Controller
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
