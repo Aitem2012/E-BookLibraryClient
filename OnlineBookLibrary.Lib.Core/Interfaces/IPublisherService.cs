70d74e1730037d44bc879b1b8432d137595292d1
Aitem2012 <ibrahim.ajumoko@decagon.dev>
2021-05-31 23:15:44 +0100

    Services updated

---
+++ OnlineBookLibrary.Lib.Core/Interfaces/IPublisherService.cs
@@ -0,0 +1,16 @@
+﻿using OnlineBookLibrary.Lib.DTO.PublisherRegister;
+using OnlineBookLibraryClient.Lib.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBookLibrary.Lib.Core.Interfaces
+{
+    public interface IPublisherService
+    {
+        public Publisher CreatePublisher(PublisherRegisterDTO model);
+    }
+}
--- OnlineBookLibrary.Lib.DTO/PublisherRegister/PublisherRegisterDTO.cs
+++ OnlineBookLibrary.Lib.DTO/PublisherRegister/PublisherRegisterDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnlineBookLibraryClient.Lib.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,6 @@ namespace OnlineBookLibrary.Lib.DTO.PublisherRegister
     public class PublisherRegisterDTO
     {
         public string PublisherName { get; set; }
+        public IEnumerable<Book> Books { get; set; }
     }
 }
