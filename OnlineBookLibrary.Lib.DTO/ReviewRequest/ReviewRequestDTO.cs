7ae40d3c3a92e8a64fb312564aa1113959dab01a
Aitem2012 <ibrahim.ajumoko@decagon.dev>
2021-06-02 15:30:23 +0100

    Review Works

---
+++ OnlineBookLibrary.Lib.DTO/ReviewRequest/ReviewRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿using OnlineBookLibraryClient.Lib.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBookLibrary.Lib.DTO.ReviewRequest
+{
+    public class ReviewRequestDTO
+    {
+        public string ReviewBody { get; set; }
+      
+        public string ReviewHeader { get; set; }
+        public AppUser   AppUser { get; set; }
+        public Book Book { get; set; }
+        public int Rating { get; set; }
+    }
+}
